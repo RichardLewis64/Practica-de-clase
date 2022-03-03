d229b1bf448670ea5a99bd74f5bc13cd05e875b6
RichardLewis64 <montesrichardmontes@gmail.com>
2022-03-02 20:28:32 -0400

    load

--- FREDAY.js
+++ FREDAY.js
@@ -7,16 +7,30 @@ function Fecha ()
       var mes = freday.getMonth();
       var year =freday.getFullYear();
  
-      let DiaSemana = document.getElementById('mostrarfecha');
-             let Dia = document.getElementById('mostrarfecha');
-             let Mes = document.getElementById('mostrarfecha');
-             let Year = document.getElementById('mostrarfecha');
+      let DiaSemana = document.getElementById('fecha');
+      document.write(Fecha)
+       let Dia = document.getElementById('day');
+       document.write(day)
+       let Mes = document.getElementById('mes');
+       document.write(mes)
+       let Year = document.getElementById('Año');
+       document.write(Año)
     
+       let dia=Fecha.getDay();
+       let mes=Fecha.getMonth();
+       let year=Fecha.getFullYear();
+
+         console.log("dia : "+dia);
+         console.log("dia : "+Mes);
+         console.log("dia : "+Año);
+        
+
              let semana = ['Domingo','Lunes','Martes','Miercoles','Jueves','Viernes','Sabado'];
              DiaSemana = semana[diaSemana];
              Dia = dia;  
              let meses = ['Enero','Febrero','Marzo','Abril','Mayo','Juio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'];
              Mes = meses[mes];
+             let = Año = [2022]
              Year = year; 
              let resultado= DiaSemana+" "+Dia+" del "+Mes+" "+" del "+ Year;
     
