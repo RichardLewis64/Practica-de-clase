072ddd7ad2382e2ba0dd4f9d049c1761616740a7
RichardLewis64 <montesrichardmontes@gmail.com>
2022-03-03 11:14:21 -0400

    load

--- SCRIP.js
+++ SCRIP.js
@@ -1,23 +1,26 @@
-for (var i = 1; i <= 100; i++) {
-   
-    
-	if (i%3==0 && i%5==0 )
-	{
-        document.writeln(i+"FizzBuzz"+"<br>");
-    }
 
-  else
-	if (i%5==0)
-	{
-        document.writeln(i+"Buzz"+"<br>");
-    }
-    else
-if(i%3==0)
+ function FizzBuzz(Num){
+    for (var i = 1; i <= Num; i++) {
+
+
+if (i%3==0 && i%5==0 )
 {
-    document.writeln(i+"Fizz"+"<br>");
+    document.writeln(i+"FizzBuzz"+"<br>");
+}
+
+else
+if (i%5==0)
+{
+    document.writeln(i+"Buzz"+"<br>");
 }
 else
-	document.write(i + '<br>');
-}     
-   
-
+if(i%3==0)
+{
+document.writeln(i+"Fizz"+"<br>");
+}
+else
+document.write(i + '<br>');
+}   
+return num
+ }
+Function (100)  
